32b4865dae7e967692b91c4ca7a6541ecd47f424
rhit-ockwigew <95239974+rhit-ockwigew@users.noreply.github.com>
2025-01-07 15:59:02 -0500

    Add files via upload

--- rfsb2.m
+++ rfsb2.m
@@ -1,4 +1,4 @@
-% This is a script written by Ethan Ockwig, last updated on 11/11/2024
+% This is a script written by Ethan Ockwig, last updated on 12/19/2024
 
 function rfsb2(A, B, name)
 % inputs are ports of A and ports of B
@@ -16,35 +16,48 @@ USB_Switch = NET.addAssembly('C:\Users\ockwigew\Downloads\mcl_RF_Switch_Controll
 
 % IP 169.254.40.176
 % Connects to device
+
 vna = visadev("TCPIP0::AGILENT-9892D87::5025::SOCKET")
-% Set the buffer size
-vna.InputBufferSize = 100000;
-% Set the timeout value
-vna.Timeout = 10;
-% Set the Byte order
-vna.ByteOrder = 'little-endian';
-% Open the connection
-fopen(vna);
+
+writeline(vna, "CALC:PAR:DEL:ALL");
+writeline(vna,"CALC1:PAR:DEF:EXT 'CH1_S11', 'S11'");
+writeline(vna, "CALC:PAR:SEL 'CH1_S11'");
+writeline(vna, "DISP:WIND:TRAC1:FEED 'CH1_S11'");
+writeline(vna,"CALC1:PAR:DEF:EXT 'CH1_S12', 'S12'");
+writeline(vna, "CALC:PAR:SEL 'CH1_S12'");
+writeline(vna, "DISP:WIND:TRAC2:FEED 'CH1_S12'");
+writeline(vna,"CALC1:PAR:DEF:EXT 'CH1_S21', 'S21'");
+writeline(vna, "CALC:PAR:SEL 'CH1_S21'");
+writeline(vna, "DISP:WIND:TRAC3:FEED 'CH1_S21'");
+writeline(vna,"CALC1:PAR:DEF:EXT 'CH1_S22', 'S22'");
+writeline(vna, "CALC:PAR:SEL 'CH1_S22'");
+writeline(vna, "DISP:WIND:TRAC4:FEED 'CH1_S22'");
+
+% writeline(vna, "MMEM:LOAD:CORR
 
 % sets external trigger
-fprintf(vna, 'TRIG:SOUR EXT');
+writeline(vna, 'TRIG:SOUR EXT');
 
-% sets the VNA to store the file as an s2p with dB
-fprintf(vna, 'MMEM:STOR:TRAC:FORM:SNP DB')
+%sets to little endian
+writeline(vna, 'FORM:BORD SWAP');
+
+% Set data type to real 64 bit binary block
+writeline(vna, 'FORM REAL,64');
 
 % sets smoothing and averaging
-%fprintf(vna, 'CALC:SMO:STAT ON');
-fprintf(vna, 'SENS:AVER:STAT ON');
-fprintf(vna, "SENS:AVER:COUN 10")
+%writeline(vna, "CALC:PAR:SEL 'CH1_S11_1'");
+writeline(vna, 'CALC:SMO ON');
+writeline(vna, 'SENS:AVER:STAT ON');
+writeline(vna, "SENS:AVER:COUN 10")
 
-fprintf(vna, 'INIT:CONT OFF');
+writeline(vna, 'INIT:CONT OFF');
  
-fprintf(vna, '*OPC?');
+writeline(vna, '*OPC?');
 
 % relay box setup
 SW1=mcl_RF_Switch_Controller_NET45.USB_RF_SwitchBox;
 SW1.Connect;
-
+readline(vna);
 
 for i = 1:length(A)
     for j = 1:length(B)
@@ -53,14 +66,27 @@ for i = 1:length(A)
         SW1.Send_SCPI(strcat('SP6TB:STATE:',num2str(B(j))),'');
 
         % clears averaging and begins new measurements
-        fprintf(vna, "SENS:AVER:CLE")
-        fprintf(vna, 'INIT:IMM');
-        fprintf(vna, '*OPC?');
+        writeline(vna, "SENS:AVER:CLE");
+        writeline(vna, 'INIT:IMM');
+        writeline(vna, '*OPC?');
 
-        % Saves S Parameters to a .s2p file
-        %fprintf(vna, strcat("DATA:SNP:PORT:SAVE'1,2 ",pwd,"\",name,"S",num2str(A(i)),num2str(B(j))));
-        fprintf(vna, '*OPC?');
-
+        % Saves the trace data to an s2p file
+        writeline(vna, 'CALC:DATA:SNP? 2');
+        data = readbinblock(vna, 'double');
+        data_r=reshape(data, [(length(data)/9),9]);
+        data_r=data_r';
+        freqs=data_r(1,:);
+        s11 = 10.^(data_r(2,:)/20).*exp(data_r(3,:).*sqrt(-1).*3.14/180);
+        s12 = 10.^(data_r(4,:)/20).*exp(data_r(5,:).*sqrt(-1).*3.14/180);
+        s21 = 10.^(data_r(6,:)/20).*exp(data_r(7,:).*sqrt(-1).*3.14/180);
+        s22 = 10.^(data_r(8,:)/20).*exp(data_r(9,:).*sqrt(-1).*3.14/180);
+        s = zeros(2,2,length(s11));
+        s(1,1,:) = s11;
+        s(1,2,:) = s12;
+        s(2,1,:) = s21;
+        s(2,2,:) = s22;
+        rfwrite(s,freqs,strcat(pwd,"\",name,"S",num2str(A(i)),num2str(B(j)),".s2p"));
+        
         % Optional: Plot the S Parameters
         figure();
         data = sparameters(strcat(name,"S",num2str(A(i)),num2str(B(j)),".s2p"));
@@ -72,27 +98,9 @@ end
 SW1.Send_SCPI('SP6TA:STATE:0','');
 SW1.Send_SCPI('SP6TB:STATE:0','');
 
-% return to internal trigger
-fprintf(vna, 'TRIG:SOUR INT');
-fprintf(vna, 'INIT:CONT ON');
+% return to preset settings
+writeline(vna, 'SYST:PRES');
 fclose(vna);
 
-end
-
-% read from VNA through TCP connection
-% blocks until data read is not empty
-% reads until new line character is received
-function query_response = VNAread(app_vna)
-    query_response = '';
-    while true
-        partial_query_response = read(app_vna);
-        if(isempty(partial_query_response)~=1)
-            last_index = length(partial_query_response);
-            query_response = strcat(query_response, partial_query_response);
-            if (partial_query_response(last_index) == 10) % 10 is newline
-                break;
-            end
-        end
-    end
 end
 
